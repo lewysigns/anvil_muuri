7a140fec866ec15b350c2b6aeb00345a13cb3233
anthonys@letemtrans.com <anthonys@letemtrans.com>
2024-01-04 05:53:18 +0000

    Edited Example.Form, settings, template, template_column and 1 more

--- client_code/Example/Form/__init__.py
+++ client_code/Example/Form/__init__.py
@@ -14,17 +14,17 @@ class Form(FormTemplate):
     # Any code you write here will run before the form opens.
     data = [
       {'header':"column 1",'background':'red','items':[ItemForm('test 1'),ItemForm('test 2')]},
-      {'header':"column 2",'background':'blue','items':[ItemForm('test 3'),ItemForm('test 6')]},
+      #{'header':"column 2",'background':'blue','items':[ItemForm('test 3'),ItemForm('test 6')]},
       #{'header':"column 3",'background':'green','items':[ItemForm('test 4'),ItemForm('test 5')]}
     ]
     for row in data:
       header = Label(text=row['header'],background=row['background'])
       column = template_column()
-      #column.add_component(header,slot="header")
-      #for item in row['items']:
-      #  I = template_item()
-      #  I.add_component(item,slot="default")
-      #  column.add_component(I,slot="content")
+      column.add_component(header,slot="header")
+      for item in row['items']:
+        I = template_item()
+        I.add_component(item,slot="item")
+        column.add_component(I,slot="content")
       self.grid.add_component(column,slot="board")
       
 
