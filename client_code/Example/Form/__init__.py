d49561ba501131fae46e0e995c69269fc7533b73
anthonys@letemtrans.com <anthonys@letemtrans.com>
2024-01-04 18:43:26 +0000

    Edited template_grid and Example.Form

--- client_code/Example/Form/__init__.py
+++ client_code/Example/Form/__init__.py
@@ -16,7 +16,7 @@ class Form(FormTemplate):
     # Any code you write here will run before the form opens.
     data = [
       {'header':"column 1",'background':'','items':[ItemForm('test 1'),ItemForm('test 2')]},
-      #{'header':"column 2",'background':'blue','items':[ItemForm('test 3'),ItemForm('test 6')]},
+      {'header':"column 2",'background':'blue','items':[ItemForm('test 3'),ItemForm('test 6')]},
       #{'header':"column 3",'background':'green','items':[ItemForm('test 4'),ItemForm('test 5')]}
     ]
     self.items = []
@@ -25,17 +25,18 @@ class Form(FormTemplate):
       header = Label(text=row['header'],background=row['background'],align='center')
       column = template_column()
       column.add_component(header,slot="header-slot")
+      column_items = []
       for item in row['items']:
         I = template_item()
         I.add_component(item,slot="item-slot")
-        self.items.append(I)
+        column_items.append(js.get_dom_node(I).querySelector(".board-item"))
         column.add_component(I,slot="content-slot")
+      self.items.append(column_items)
       self.columns.append(column)
       self.grid.add_component(column,slot="board-slot")
       
 
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
-    items = [js.get_dom_node(i).querySelector(".board-item") for i in self.items]
-    board = [js.get_dom_node(self.grid).querySelector(".board-column") for c in self.columns]
-    self.grid.create_board(items,board)
+    columns = [js.get_dom_node(c).querySelector(".board-column") for c in self.columns]
+    self.grid.create_board(self.items,columns)
